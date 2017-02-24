d9efa8174c4285045a4de06d650b768d248c7b77
Agtoutzi <grandthau@hotmail.com>
2017-02-24 20:30:15 +0200

    Modify mna files

--- mna.c
+++ mna.c
@@ -5,6 +5,11 @@
 #include "CircLib.h"
 #include "hash.h"
 #include "mna.h"
+#include "options.h"
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_cblas.h>
+
+
 
 void CreateMna(){
 
@@ -17,20 +22,15 @@ void CreateMna(){
 	int i,j;
 	int b=1;
 	int n= hash_count-1;
+	double temp;
 
 	sizeA = (hash_count-1)+m2;
 	
-	A = (double**)calloc(sizeA, sizeof(double*));				//na mpei elegxos gia calloc
-	temp = (double*)calloc(pow(sizeA,2), sizeof(double));
+	A = gsl_matrix_calloc(sizeA,sizeA);
 	
-	for (i = 0; i < sizeA; i++) {
-  		A[i] = temp + (i * sizeA);
-	}
-		
 	sizeB = (hash_count-1)+m2;
-	B = (double*)calloc(sizeB, sizeof(double));		//na mpei elegxos gia calloc
-	
-	
+	B = gsl_vector_calloc(sizeB);
+
 	//Diatrexoume ti lista twn antistasewn kai simplirwnoume katallila to 1o n-1 * n-1 kommati tou pinaka A
 	
 	ResistanceT *currentR=rootR;
@@ -39,11 +39,19 @@ void CreateMna(){
 		i=atoi(ht_get(hashtable,currentR->node1));
 		j=atoi(ht_get(hashtable,currentR->node2));
 		
-		if(i!=0){A[i-1][i-1]+=1/currentR->value;}
-		if(j!=0){A[j-1][j-1]+=1/currentR->value;}
+		if(i!=0){
+		  temp=gsl_matrix_get (A, i-1, i-1);
+		  gsl_matrix_set (A, i-1, i-1, temp + 1/currentR->value);
+		}
+		if(j!=0){
+		  temp=gsl_matrix_get (A, j-1, j-1);
+		  gsl_matrix_set (A, j-1, j-1, temp + 1/currentR->value);
+		}
 		if(i!=0&&j!=0){
-			A[i-1][j-1]-=1/currentR->value;
-			A[j-1][i-1]-=1/currentR->value;
+			temp=gsl_matrix_get (A, i-1, j-1);
+			gsl_matrix_set (A, i-1, j-1, temp - 1/currentR->value);
+			temp=gsl_matrix_get (A, j-1, i-1);
+			gsl_matrix_set (A, j-1, i-1, temp - 1/currentR->value);
 		}
 		currentR=currentR->next;
 	}
@@ -57,12 +65,12 @@ void CreateMna(){
 	      i = atoi(ht_get(hashtable,currentV->node1));    //vlepoume metaksu poiown komvwn vrisketai h phgh tashs 
 	      j = atoi(ht_get(hashtable,currentV->node2));
 	
-	      if(i!=0){A[n-1+b][i-1]=1;}		   //grammh-sthlh
-	      if(j!=0){A[n-1+b][j-1]=-1;}
+	      if(i!=0){gsl_matrix_set (A, n-1+b, i-1, 1.000);}		   //grammh-sthlh
+	      if(j!=0){gsl_matrix_set (A, n-1+b, j-1, -1.000);}
 	      
-	      if(i!=0){A[i-1][n-1+b]=1;}		//sthlh-grammh
-	      if(j!=0){A[j-1][n-1+b]=-1;}
-	      if((i!=0)||(j!=0)){B[n-1+b]=currentV->value;}	//vazw ston B tis times twn tasewn
+	      if(i!=0){gsl_matrix_set (A, i-1, n-1+b, 1.000);}		//sthlh-grammh
+	      if(j!=0){gsl_matrix_set (A, j-1, n-1+b, -1.000);}
+	      if((i!=0)||(j!=0)){gsl_vector_set (B, n-1+b, currentV->value);}	//vazw ston B tis times twn tasewn
 	      
 	      b++;
 	      currentV= currentV ->next;
@@ -76,11 +84,11 @@ void CreateMna(){
 	      i = atoi(ht_get(hashtable,currentL->node1));    //vlepoume metaksu poiown komvwn vrisketai to pinio 
 	      j = atoi(ht_get(hashtable,currentL->node2));
 	
-	      if(i!=0){A[n-1+b][i-1]=1;}		   //grammh-sthlh
-	      if(j!=0){A[n-1+b][j-1]=-1;}
+	      if(i!=0){gsl_matrix_set (A, n-1+b, i-1, 1.000);}		   //grammh-sthlh
+	      if(j!=0){gsl_matrix_set (A, n-1+b, j-1, -1.000);}
 	      
-	      if(i!=0){A[i-1][n-1+b]=1;}		//sthlh-grammh
-	      if(j!=0){A[j-1][n-1+b]=-1;}
+	      if(i!=0){gsl_matrix_set (A, i-1, n-1+b, 1.000);}		//sthlh-grammh
+	      if(j!=0){gsl_matrix_set (A, j-1, n-1+b, -1.000);}
  
 	      b++;
 	      currentL= currentL ->next;
@@ -94,8 +102,14 @@ void CreateMna(){
 	      i = atoi(ht_get(hashtable,currentI->node1));    //vlepoume metaksu poiown komvwn vrisketai h phgh tashs 
 	      j = atoi(ht_get(hashtable,currentI->node2));
 	
-	      if(i!=0){B[i-1]-=currentI->value;}
-	      if(j!=0){B[j-1]+=currentI->value;}
+	      if(i!=0){
+		temp=gsl_vector_get(B,i-1);
+		gsl_vector_set (B, i-1, temp - currentI->value);
+	      }
+	      if(j!=0){
+		temp=gsl_vector_get(B,j-1);
+		gsl_vector_set (B, j-1, temp + currentI->value);
+	      }
 	      
 	      currentI = currentI ->next;
 	
@@ -108,14 +122,14 @@ void CreateMna(){
 	for(i=0;i<sizeA;i++){
 		for(j=0;j<sizeA;j++){
 
-			printf(" %.3lf ",A[i][j]);
+			printf(" %.3lf ",gsl_matrix_get (A, i, j));
 		}
 		printf("\n");
 	}
 
 	printf(" B\n");
 	for(i=0;i<sizeB;i++){
-		printf(" %.3lf ",B[i]);
+		printf(" %.3lf ",gsl_vector_get(B,i));
 	}
 
 	printf("\n");
